35c355897ecc5b8d9faed1e3bbb37eda49175ffb
Rohit Saluja <saluja730@gmail.com>
2025-07-04 20:00:56 +0530

    updated the page of this is done

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -41,15 +41,15 @@ const Navbar = () => {
     <motion.div className="flex items-center gap-7 absolute inset-x-0 justify-center" layoutId="navbar">
       <button className="text-sm text-neutral-300 relative px-3 py-2" onMouseEnter={() => setActive("features")} onMouseLeave={() => setActive("")}>
         <span>Features</span>
-        {active === "features" && <div className="absolute bg-neutral-700 inset-0 rounded-full -z-10 opacity-50"></div>}
+        {active === "features" && <motion.div className="absolute bg-neutral-700 inset-0 rounded-full -z-10 opacity-50" layoutId="navbar-active"></motion.div>}
       </button>
       <button className="text-sm text-neutral-300 relative px-3 py-2" onMouseEnter={() => setActive("pricing")} onMouseLeave={() => setActive("")}>
         <span>Pricing</span>
-        {active === "pricing" && <div className="absolute bg-neutral-700 inset-0 rounded-full -z-10 opacity-50"></div>}
+        {active === "pricing" && <motion.div className="absolute bg-neutral-700 inset-0 rounded-full -z-10 opacity-50" layoutId="navbar-active"></motion.div>}
       </button>
       <button className="text-sm text-neutral-300 relative px-3 py-2" onMouseEnter={() => setActive("contact")} onMouseLeave={() => setActive("")}>
         <span>Contact</span>
-        {active === "contact" && <div className="absolute bg-neutral-700 inset-0 rounded-full -z-10 opacity-50"></div>}
+        {active === "contact" && <motion.div className="absolute bg-neutral-700 inset-0 rounded-full -z-10 opacity-50" layoutId="navbar-active"></motion.div>}
       </button>
     </motion.div>
   );
