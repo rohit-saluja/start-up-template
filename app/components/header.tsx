35586c51d5e22b459ab201e6d6f091d9b41821e8
Rohit Saluja <saluja730@gmail.com>
2025-07-04 19:49:30 +0530

    updated the header of this is done

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,6 +1,9 @@
+"use client";
 import { Moon } from "lucide-react";
+import { useState } from "react";
 
 export default function Header() {
+  const [active, setActive] = useState<"features" | "pricing" | "contact" | "">("");
   return (
     <div className="flex items-center justify-between max-w-7xl mx-auto py-3 relative">
       <div className="flex items-center gap-2">
@@ -15,9 +18,18 @@ export default function Header() {
         <p className="text-sm font-semibold text-neutral-100">Startup</p>
       </div>
       <div className="flex items-center gap-7 absolute inset-x-0 justify-center">
-        <p className="text-sm text-neutral-300">Features</p>
-        <p className="text-sm text-neutral-300">Pricing</p>
-        <p className="text-sm text-neutral-300">Contact</p>
+        <button className="text-sm text-neutral-300 relative px-3 py-2" onMouseEnter={() => setActive("features")} onMouseLeave={() => setActive("")}>
+          <span>Features</span>
+          {active === "features" && <div className="absolute bg-neutral-700 inset-0 rounded-full -z-10 opacity-50"></div>}
+        </button>
+        <button className="text-sm text-neutral-300 relative px-3 py-2" onMouseEnter={() => setActive("pricing")} onMouseLeave={() => setActive("")}>
+          <span>Pricing</span>
+          {active === "pricing" && <div className="absolute bg-neutral-700 inset-0 rounded-full -z-10 opacity-50"></div>}
+        </button>
+        <button className="text-sm text-neutral-300 relative px-3 py-2" onMouseEnter={() => setActive("contact")} onMouseLeave={() => setActive("")}>
+          <span>Contact</span>
+          {active === "contact" && <div className="absolute bg-neutral-700 inset-0 rounded-full -z-10 opacity-50"></div>}
+        </button>
       </div>
       <div className="flex items-center gap-5">
         <button>
